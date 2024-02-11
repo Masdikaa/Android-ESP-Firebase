3638bfc2fb91ceffd85ed28a39954388a9ce2b9b
Masdikaa <masdikailhanmansiz@gmail.com>
2024-02-11 11:48:13 +0700

    Bind project to Firebase

--- app/src/main/java/com/masdika/espandroidfirebase/MainActivity.kt
+++ app/src/main/java/com/masdika/espandroidfirebase/MainActivity.kt
@@ -3,16 +3,20 @@ package com.masdika.espandroidfirebase
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.firebase.database.DatabaseReference
 import com.masdika.espandroidfirebase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
     }
+
 }
